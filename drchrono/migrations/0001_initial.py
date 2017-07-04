c793e1720d1926d87eae696c32568315cfeb83c7
Lucas Reynolds <reyno392@Melissas-MacBook-Air.local>
2017-07-04 18:51:57 -0400

    got drchrono api working with psa.

---
+++ drchrono/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import phonenumber_field.modelfields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Docter',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=50)),
+                ('last_name', models.CharField(max_length=50)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Patient',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=50)),
+                ('last_name', models.CharField(max_length=50)),
+                ('email', models.EmailField(max_length=254)),
+                ('phone', phonenumber_field.modelfields.PhoneNumberField(max_length=128)),
+                ('doctor', models.ForeignKey(to='drchrono.Docter')),
+            ],
+        ),
+    ]
